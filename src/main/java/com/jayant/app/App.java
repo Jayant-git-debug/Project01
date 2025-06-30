26920305adbd1e43826af4e0a66287c8416281b5
Jayant <jayantbasone100@yahoo.com>
2025-07-01 01:12:18 +0530

    Jayant

---
+++ src/main/java/com/jayant/app/App.java
@@ -0,0 +1,10 @@
+package com.jayant.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello this project built for the World!");
+    }
+}
